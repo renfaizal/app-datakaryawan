7bdd9f7670499ce5ceaf2bca6ff913e1f700fe4a
renfaizal <ren.faizal@gmail.com>
2019-03-29 15:24:46 +0700

    upload initial project

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Body from './components/Body';
-import Footer from './components/Footer';
+import React from 'react'
+import Layout from './app/Layout'
 
-
-
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Navbar />
-        <Route exact path="/components/Home" component={Home} />
-        <Route exact path="/components/About" component={About} />
-        <Route exact path="/components/Contact" component={Contact} />
-        <hr />
-        <Footer />
-
-
-
-      </div>
+      <Layout />
     );
   }
 }
 
-export default App;
+export default App
